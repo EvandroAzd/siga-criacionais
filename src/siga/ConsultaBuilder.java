6b22aac523c10a34fd9a636be31a398f0903c374
evandro <azevedoevandrogabriel@gmail.com>
2026-09-15 21:33:20 -0300

    Construindo o metodo build

--- src/siga/ConsultaBuilder.java
+++ src/siga/ConsultaBuilder.java
@@ -47,5 +47,15 @@ public class ConsultaBuilder {
         return this;
     }
 
+    public String build() {
+        StringBuilder sb = new StringBuilder("SELECT * FROM ").append(tabela);
+        if (filtro != null) sb.append(" WHERE ").append(filtro);
+        if (somenteAtivos) sb.append(filtro != null ? " AND ativo = 1" : " WHERE ativo = 1");
+        if (ordenacao != null) sb.append(" ORDER BY ").append(ordenacao);
+        if (limite > 0) sb.append(" LIMIT ").append(limite);
+        if (offset > 0) sb.append(" OFFSET ").append(offset);
+        return sb.toString();
+    }
+
 
 }
